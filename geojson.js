8faa0018c794fc2c4908ffade85b83a044c3092d
ClaireJimenez <72587006+ClaireJimenez@users.noreply.github.com>
2020-10-08 21:03:32 -0500

    UpdateGeojson

--- geojson.js
+++ geojson.js
@@ -17,33 +17,33 @@ var json = {
         "stroke-opacity": 1,
         "fill": "#555555",
         "fill-opacity": 0.5,
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg",
-        "title": "Nebraska State Capitol",
-        "description": "Some description about something that happened at the capitol"
+        "Title": "Lied Center Courtyard",
+        "Image": "https://1.bp.blogspot.com/-EcTyDnwVySo/X3-dU18EJdI/AAAAAAAAC4w/LEMP22QZbowulsr9g5fQyocplRBojWluACLcBGAsYHQ/s320/EmptyCourtyardLiedCenter.jpg",
+        "Description": "Empty Courtyard on a Sunday Afternoon"
       },
       "geometry": {
         "type": "Polygon",
         "coordinates": [
           [
             [
-              -96.70105934143066,
-              40.806971850092815
+              -96.70473396778107,
+              40.81666740406686
             ],
             [
-              -96.69826984405518,
-              40.806971850092815
+              -96.7042726278305,
+              40.81666740406686
             ],
             [
-              -96.69826984405518,
-              40.809131953785965
+              -96.7042726278305,
+              40.816858215242206
             ],
             [
-              -96.70105934143066,
-              40.809131953785965
+              -96.70473396778107,
+              40.816858215242206
             ],
             [
-              -96.70105934143066,
-              40.806971850092815
+              -96.70473396778107,
+              40.81666740406686
             ]
           ]
         ]
@@ -52,61 +52,44 @@ var json = {
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#ee05ff",
+        "marker-color": "#7e7e7e",
         "marker-size": "medium",
         "marker-symbol": "",
-        "ingredients": "rhubarb, strawberries, egg, milk, butter, so much sugar",
-        "thanks": "thanks to Linnea for giving me a pie",
-        "title": "Starting Point",
-        "image": "https://s3.amazonaws.com/omeka-net/53984/archive/square_thumbnails/e5847940c8baa5e9e50ad71ee468ba0e.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1600905600&Signature=PapYafLVt%2BGfMiI%2F06Ibpay%2FXPk%3D"
+        "Title": "Marcus Lincoln Grand Cinema",
+        "Image": "https://1.bp.blogspot.com/-U5Zg02PahO8/X3-dYxe2c-I/AAAAAAAAC40/McD0tu3gok87rYeXEcD2-GFa6STuWgoQACLcBGAsYHQ/s320/EmptyParkingSpotsMovie.jpg",
+        "Description": "Empty Parking Spots"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70299053192137,
-          40.81015513625622
+          -96.70410633087158,
+          40.814418226723134
         ]
       }
     },
     {
       "type": "Feature",
-      "properties": {
-        "stroke": "#ff5100",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "title": "Path to the library",
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg"
-      },
+      "properties": {},
       "geometry": {
-        "type": "LineString",
+        "type": "Polygon",
         "coordinates": [
           [
-            -96.69979333877563,
-            40.80922127986828
-          ],
-          [
-            -96.69986844062804,
-            40.811389430620544
-          ],
-          [
-            -96.6997504234314,
-            40.811405671182825
-          ],
-          [
-            -96.6996967792511,
-            40.81468618325975
-          ],
-          [
-            -96.69984698295592,
-            40.8146943031379
-          ],
-          [
-            -96.69984698295592,
-            40.816837916207085
-          ],
-          [
-            -96.70262575149535,
-            40.81687851427114
+            [
+              -96.7052435874939,
+              40.815173374189946
+            ],
+            [
+              -96.7052435874939,
+              40.815173374189946
+            ],
+            [
+              -96.7052435874939,
+              40.815173374189946
+            ],
+            [
+              -96.7052435874939,
+              40.815173374189946
+            ]
           ]
         ]
       }
@@ -114,66 +97,60 @@ var json = {
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#d4ff00",
+        "stroke": "#555555",
         "stroke-width": 2,
         "stroke-opacity": 1,
-        "fill": "#0060ff",
-        "fill-opacity": 0.2,
-        "title": "Love Library",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/189753833@N07/50237618773/in/album-72157715528007683/\" title=\"yawn_connie\"><img src=\"https://live.staticflickr.com/65535/50237618773_c9052299b5_w.jpg\" width=\"300\" height=\"400\" alt=\"yawn_connie\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "random": "Some other random field"
+        "fill": "#555555",
+        "fill-opacity": 0.5,
+        "Title": "Lincoln Community Foundation Tower Square"
       },
       "geometry": {
         "type": "Polygon",
         "coordinates": [
           [
             [
-              -96.70274376869202,
-              40.817040906278805
+              -96.70244872570038,
+              40.814840460774725
             ],
             [
-              -96.70216441154479,
-              40.81700030831414
+              -96.7020732164383,
+              40.814840460774725
             ],
             [
-              -96.70187473297119,
-              40.817235776163265
+              -96.7020732164383,
+              40.81530735106853
             ],
             [
-              -96.70193910598755,
-              40.81753619944697
+              -96.70244872570038,
+              40.81530735106853
             ],
             [
-              -96.70217514038086,
-              40.81769858984467
-            ],
-            [
-              -96.70218586921692,
-              40.818104564099706
-            ],
-            [
-              -96.70297980308533,
-              40.81808832517719
-            ],
-            [
-              -96.70300126075745,
-              40.817609275175144
-            ],
-            [
-              -96.70323729515076,
-              40.81742252593206
-            ],
-            [
-              -96.70299053192137,
-              40.81704902586876
-            ],
-            [
-              -96.70274376869202,
-              40.817040906278805
+              -96.70244872570038,
+              40.814840460774725
             ]
           ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Title": "Welcome Center",
+        "Image": "https://1.bp.blogspot.com/-cWqNdYv_GVg/X3-dQGDtryI/AAAAAAAAC4o/6u6MmXTseBs2KXI56iqDSg9YJLgtf2RjgCLcBGAsYHQ/s320/BikesWelcomeCenter.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70353233814238,
+          40.815981291263206
         ]
       }
     }
   ]
 }
+   
+     
+            
